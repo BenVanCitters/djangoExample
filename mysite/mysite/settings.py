a8667b1c031c489725a73f868b3459a144f77b92
Ben V <van.citters.b@gmail.com>
2019-08-22 16:29:06 -0500

    changed settings and ran a migration

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_poll_test',
+        'USER':'poll_django_user',
+        'PASSWORD':'django',
     }
 }
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
